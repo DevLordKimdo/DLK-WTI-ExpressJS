fc9eaa5e22099d954b699803afd066fb4911078b
김동오 <devlordkimdo@gmail.com>
2025-04-06 22:24:07 +0900

    first Commit

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+// 빌드 명령어 (타입스크립트로 작성된 코드를 자바스크립트 코드로 변환해주는 과정)
+// npm run build 
+
+// 프로젝트 시작 명령어
+// pm2 start ecosystem.config.js --env dev
+// pm2 start ecosystem.config.js --env prod
+
+module.exports = {
+    apps: [
+        {
+            name       :  'DLK-WTI-ExpressJS',  // PM2에서 표시될 애플리케이션 이름
+            script     :  './dist/index.js',    // 실행할 스크립트 경로 (컴파일된 JS 파일)
+            watch      :  false,                // 파일 변경 감지 자동 재시작 기능
+            instances  :  1,                    // 생성할 애플리케이션 인스턴스 수. CPU 프로세스 개수에 따라 할당이 가능. "MAX"값을 하면 프로세스 최대로 가동.
+            exec_mode  :  'cluster',            // 실행 모드 (cluster 또는 fork) fork로 설정하면 instances 값은 무시하고 단일로 설정함
+            env_dev    :  { NODE_ENV : 'development', APP_ENV : 'dev',  },  // 개발 환경 변수
+            env_prod   :  { NODE_ENV : 'production',  APP_ENV : 'prod', },  // 프로덕션 환경 변수
+        }
+    ]
+};
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import config  from 'config';
+
+const app     = express();
+
+const svrConf = config.get('server')   as { port: number };
+const svrPort = svrConf.port;
+
+const dbConf  = config.get('database') as { port: number };
+const dbPort = svrConf.port;
+
+console.log(`Express TypeScript Demo 접속 완료.`);
+console.log(`서버 접속포트 번호 : ${svrPort}`);
+console.log(`DB 포트 정보 : ${dbPort}`);
+
+app.get('/', async (req, res) => { 
+    console.log('incoming index page');
+    res.json('index');
+});
+
+app.listen(svrPort);

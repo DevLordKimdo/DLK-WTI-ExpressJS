0c3ec3b00988ff583642cf3ba4060e5ff51299e2
김동오 <devlordkimdo@gmail.com>
2025-04-23 15:57:44 +0900

    Db Form Rowsubmit 추가

--- src/index.js
+++ src/index.js
@@ -38,16 +38,17 @@ app.use(session({
     secret            : '1234', // 세션 암호화 키
     resave            : false,  // 세션이 변경되지 않았더라도 다시 저장할지 여부
     saveUninitialized : false,  // 초기화되지 않은 세션을 저장할지 여부
-    store: new SQLiteStore({
-        db: ':memory:', // 메모리 모드 지정
-        table: 'sessions' // 테이블 이름 (선택사항)
-    })
+    store             : new SQLiteStore(
+        { db: ':memory:' }                  // 세션 저장소 설정. SQLite 의 메모리 저장소 모듈을 사용. (메모리모드)
+    //  { db: 'sessions.db', dir: './db' }  // (로컬모드, 경로는 './db')
+    )
 }));
 
 // 라우팅 매핑
 app.use('/'                      , require(global.rootPath + '/routes/main/main_rts'));
 app.use('/be/http/prg-pattern'   , require(global.rootPath + '/routes/be/http/prgpattern/beHttpPrgpattern_rts'));
 app.use('/db/crud/basic'         , require(global.rootPath + '/routes/db/crud/basic/dbCrudBasic_rts'));
+app.use('/db/form/row-submit'    , require(global.rootPath + '/routes/db/form/rowsubmit/dbFormRowsubmit_rts.js'));
 app.use('/fio/crud/basic'        , require(global.rootPath + '/routes/fio/crud/basic/fioCrudBasic_rts'));
 app.use('/fe/form/input-disable' , require(global.rootPath + '/routes/fe/form/inputdisable/feFormInputdisable_rts.js'));
 app.use('/fe/form/row-submit'    , require(global.rootPath + '/routes/fe/form/rowsubmit/feFormRowsubmit_rts.js'));
---
+++ src/routes/db/form/rowsubmit/dbFormRowsubmit_rts.js
@@ -0,0 +1,28 @@
+const express        = require('express');
+const router         = express.Router();
+const dbCrud_mdl     = require(global.rootPath + '/models/dbCrud_mdl');
+
+router.get(['/','/form'], async (req, res) => {
+
+    res.render('db/form/rowsubmit/form', {});
+});
+
+router.post('/submit', async (req, res) => {
+
+    let listTitle    = req.body.title;
+    let listName     = req.body.name;
+    let listContent  = req.body.content;
+
+    for(let i = 0; i < listTitle.length; i++) {
+        let form     = {};
+        form.title   = listTitle[i];
+        form.name    = listName[i];
+        form.content = listContent[i];
+
+        dbCrud_mdl.create(form);
+    }
+
+    res.redirect('/db/form/row-submit/form');
+});
+
+module.exports = router;
